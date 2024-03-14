456dc2412c9ea1501472a33bfd1ce1ceb7c18cd1
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2024-03-14 02:32:11 -0400

    Update head.php
    Signed-off-by: ArchBlood <35392110+ArchBlood@users.noreply.github.com>

--- views/layouts/head.php
+++ views/layouts/head.php
@@ -3,6 +3,20 @@
 <meta name="application-name" content="<?= \yii\helpers\Html::encode(Yii::$app->name) ?>">
 <!-- end: Web App Data-->
 
+<!-- start: Font -->
+<link href="<?= $this->theme->getBaseUrl() . '/fonts/material-icons.css'; ?>" rel="stylesheet">
+<!-- end: Font -->
+
+<!-- start: CSS -->
+<link href="<?= $this->theme->getBaseUrl() . '/css/animate.css'; ?>" rel="stylesheet">
+<link href="<?= $this->theme->getBaseUrl() . '/css/flatelements.css'; ?>" rel="stylesheet">
+<link href="<?= $this->theme->getBaseUrl() . '/css/tidy.css'; ?>" rel="stylesheet">
+<!-- end: CSS -->
+
+<!-- start: JS file imports -->
+<?php $this->registerJsFile($this->theme->getBaseUrl() . '/js/script.js'); ?>
+<!-- end: JS file imports -->
+
 <!-- start: Favicon and Touch Icons -->
 <link rel="apple-touch-icon" sizes="57x57" href="<?= $this->theme->getBaseUrl(); ?>/ico/apple-icon-57x57.png">
 <link rel="apple-touch-icon" sizes="60x60" href="<?= $this->theme->getBaseUrl(); ?>/ico/apple-icon-60x60.png">
